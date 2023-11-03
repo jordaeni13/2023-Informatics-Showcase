e892ac624b655abf092a22fca43362e96e1aa1f8
sunsh9876543210 <sunsh9876543210@gmail.com>
2023-11-03 11:25:37 +0800

    Update MonitorHandler.cs

--- 2023 Informatics Showcase/Assets/Scripts/MonitorHandler.cs
+++ 2023 Informatics Showcase/Assets/Scripts/MonitorHandler.cs
@@ -5,13 +5,8 @@ using UnityEngine;
 public class MonitorHandler : MonoBehaviour
 {
     // Start is called before the first frame update
-    UnityEngine.Video.VideoClip Clip = null;
-    List<UnityEngine.Video.VideoClip> Clips = new List<UnityEngine.Video.VideoClip>();
     void Start()
     {
-        GetComponent<UnityEngine.Video.VideoPlayer>().clip = null;
-        Clip = null;
-
     }
 
     // Update is called once per frame
